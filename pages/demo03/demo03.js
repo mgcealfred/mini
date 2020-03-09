3a8028ea92c72c898b8f5d71d409bd47b79d5957
mashuang <mgceshuang@sina.com>
2020-03-09 09:46:02 +0800

    init

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,90 @@
+// pages/demo03/demo03.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg:"hello mina",
+    num:1000,
+    isGirl:false,
+    person:{
+      age:84,
+      height:170,
+      weight:50,
+      name:"司马"
+    },
+    isChecked:false,
+    list:[
+      {
+        id: 1,
+        name: "刘备"
+      },
+      {
+        id: 2,
+        name: "关羽"
+      },
+      {
+        id: 3,
+        name: "赵云"
+      }
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
